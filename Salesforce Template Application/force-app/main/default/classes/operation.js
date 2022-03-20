7b413c221b93fd13a5501088238c4af20d2fec8a
GirishRed <usermahen13@gmail.com>
2022-03-20 08:34:50 +0530

    Javascript Assignment Girish

---
+++ Salesforce Template Application/force-app/main/default/classes/operation.js
@@ -0,0 +1,16 @@
+var str = "Hello World!";
+var n = str.length;
+console.log(n);
+
+var str1 = "Hello ";
+var str2 = "world!";
+var res = str1.concat(str2);
+console.log(res)
+
+
+const paragraph = 'The dog quick brown fox jumps over the lazy . If the dog barked, was it really lazy';
+
+const searchTerm = 'dog';
+const indexOfFirst = paragraph.indexOf(searchTerm);
+
+console.log(`The index of the first "${searchTerm}" from the beginning is ${indexOfFirst}`);
