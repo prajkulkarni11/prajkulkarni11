f92e7e31a3ee67562a51c3249a2fc55cbccd648d
SwapnilThalal <swapnilnthalal@gmail.com>
2022-03-23 11:04:17 +0530

    Swapnil Lwc Assignment Completed 23-03-22

---
+++ Salesforce Template Application/force-app/main/default/lwc/currencyConverter/currencyConverter.js
@@ -0,0 +1,62 @@
+
+/**
+ * @File Name          : CurrencyConverter.js
+ * @Description        : Collection Assignment10Create currency convert LWC component by make callout for conversion rate.
+Please find below example to design the LWC Component.
+
+ * @Author             : Swapnil Thalal
+ * @Group              : 
+ * @Last Modified By   : Swapnil Thalal
+ * @Last Modified On   : 23/03/2022, 9:40:39 am
+ * @Modification Log   : 
+ 
+**/
+import { LightningElement ,track} from 'lwc';
+import getRates from '@salesforce/apex/CurrencyConverterController.getRates'
+export default class CurrencyConverter extends LightningElement {
+    @track rates = []
+    @track currencyOptions = []
+    @track amount = 1
+    @track baseCurrency = 'EUR'
+    @track targetCurrency = 'GBP'
+    @track convertedValue
+
+    // Fetch our rates from apex, mark `selected` accordingly (select option consumption)
+    fetchRates = () => {
+        getRates({baseCurrency: this.baseCurrency})
+            .then(data => {
+                for (let currency in data) {
+                    if (data.hasOwnProperty(currency)) {
+                        this.rates = [...this.rates, { currency, value: data[currency], selected: currency === this.targetCurrency }]
+                    }
+                }
+            })
+            .catch(error => console.error(error))
+    }
+
+    connectedCallback() {
+        this.fetchRates()
+    }
+
+    // Has no used for now, as our API key is only capable of using 'EUR' as base currency
+    handleOnBaseCurrencyChange = (e) => {
+        this.baseCurrency = e.target.value
+        this.fetchRates()
+    }
+
+    // For amount and target currency input boxes
+    handleOnInputChange = (e) => {
+        this[e.target.name] = e.target.value
+        this.convertedValue = null
+    }
+
+    // Find our rate for that currency then do the conversion
+    handleOnFormSubmit = (e) => {
+        e.preventDefault();
+        
+        const targetRate = this.rates.find(r => r.currency === this.targetCurrency)
+        if (targetRate) {
+            this.convertedValue = this.amount * targetRate.value
+        }
+    }
+}
